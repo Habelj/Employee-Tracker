f38bc862c56e498fc1cdd420638bb0cbcaf55e03
Habelj <133816606+Habelj@users.noreply.github.com>
2023-09-22 12:10:09 -0700

    "changed password in index.js"

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const figlet = require('figlet');
 
 const connection = mysql.createConnection({
   host: 'localhost',
-  port: 3001,
+  port: 3306,
   user: 'root',
   password: 'water',
   database: 'employee_DB',
